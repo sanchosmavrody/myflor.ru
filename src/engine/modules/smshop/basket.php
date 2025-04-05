b240249986b58b297cbb02bfc6bba42147cf4e09
SanchosMavrody <sanchosmavrody@gmail.com>
2025-04-05 20:30:28 +0300

    ref

--- src/engine/modules/smshop/basket.php
+++ src/engine/modules/smshop/basket.php
@@ -1,33 +1,5 @@
 <?php
 
-$Basket = new Basket('shop_basket');
-
-$state = ['pager' => ['current' => 0, 'limit' => 100], 'sorter' => [], 'filter' => [], 'grouper' => []];
-
-$Res = $Basket->getList($state['filter'], $state['pager']);
-
-$state['pager']['filtered'] = $state['pager']['total'] = $Res['pager']['filtered'];
-
-
-$tpl->load_template('/smshop/basket/shortstory.tpl');
-
-foreach ($Res['data'] as &$item) {
-
-    foreach ($item as $field => $value)
-        $tpl->set('{' . $field . '}', $value);
-
-    $tpl->set('{shop_catalog}', $shop_catalog);
-    $tpl->compile('items');
-}
-
-// header('Content-Type: application/json; charset=UTF-8');
-// echo json_encode($Res);
-// exit();
-
-include 'navigation.php';
-
-$tpl->load_template('/smshop/basket/main.tpl');
-$tpl->set('{shop_catalog}', $shop_catalog);
-$tpl->set('{items}', $tpl->result['items']);
-$tpl->set('{navigation}', $tpl->result['navigation']);
+$tpl->load_template('/smshop/basket.tpl');
+$tpl->set('{shop_catalog}', 'shop_catalog');
 $tpl->compile('content');
